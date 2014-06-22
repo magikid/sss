04655b9ff2402282f68f95c89a2dee81455b6aed
Chris Jones <chris@christopherjones.us>
2014-06-22 11:30:39 -0400

    Finished updating CSSCompilationSpec.scala to be fully in Scala.  Now I just need to figure out how to get the parser working.

--- test/CSSCompilationSpec.scala
+++ test/CSSCompilationSpec.scala
@@ -1,16 +1,25 @@
 import collection.mutable.Stack
 import org.scalatest._
 
-class CSSCompilationSpec extends FunSpec {
+class CSSCompilationSpec extends FunSpec with Matchers {
+
+  def fixture = 
+    new {
+      val parser = new Parser
+    }
+
   describe("Compilation to CSS") {
     it("should compile empty rule") {
-      val stack = "h1 {  }\n"+
+      val f = fixture
+      val code = "h1 {  }\n"+
                   "p {  }"
-      assert(stack == parser.parse)
+      code should equal (f.parser.parse(code).toCSS())
     }
 
     it ("should compile properties") {
-      assert(false)
+      val f = fixture
+      val code = "h1 { font-size: 10px; padding: 10px 20px; }"
+      code should equal (f.parser.parse(code).toCSS())
     }
   }
 }
